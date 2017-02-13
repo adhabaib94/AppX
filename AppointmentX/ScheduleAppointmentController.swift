0a04deab5e55840f160056fdad58f29e6fb289db
Abdullah Al Dhabaib <abdullahaldhabaib@aldhabaib94.local>
2017-02-14 00:18:30 +0300

    UI-Design-Appointment-v2

--- AppointmentX/ScheduleAppointmentController.swift
+++ AppointmentX/ScheduleAppointmentController.swift
@@ -17,13 +17,14 @@ class ScheduleAppointmentController: UIViewController, FSCalendarDataSource, FSC
     @IBOutlet weak var slot3_button: UIButton!
     @IBOutlet weak var slot2_button: UIButton!
     
+    @IBOutlet weak var timeTravelLabel: UILabel!
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
     override func viewDidLoad() {
-       
+        
         // Calender Delegation
         calender.dataSource = self
         calender.delegate = self
@@ -31,8 +32,6 @@ class ScheduleAppointmentController: UIViewController, FSCalendarDataSource, FSC
         calender.appearance.weekdayFont =  UIFont(name: "SourceSansPro-Regular", size: 20)
         calender.appearance.titleFont =  UIFont(name: "SourceSansPro-Regular", size: 14)
         let today = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
         calender.select(today)
         
         
@@ -44,7 +43,15 @@ class ScheduleAppointmentController: UIViewController, FSCalendarDataSource, FSC
         self.slot2_button.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
         self.slot3_button.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
         
-
+        //Time Travel Label Setup
+        let myStr = "1F60A"
+        let str = String(Character(UnicodeScalar(Int(myStr, radix: 16)!)!))
+        let final_str = "Hmm, Do you have a time machine? " + str
+        self.timeTravelLabel.text = final_str
+        self.timeTravelLabel.alpha = 0
+        self.timeTravelLabel.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -65,7 +72,17 @@ class ScheduleAppointmentController: UIViewController, FSCalendarDataSource, FSC
         })
     }
     
-    func hideAllSlotButtons(){
+    func showTimeTravelLabel(){
+        UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.timeTravelLabel.alpha = 1
+            self.timeTravelLabel.transform = CGAffineTransform(scaleX: 1, y: 1)
+            
+        }, completion: { (Bool) in
+        })
+        
+    }
+    
+    func hideAllSlotButtons(reverse: Bool){
         UIView.animate(withDuration: 0.2, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
             self.slot1_button.alpha = 0
             self.slot2_button.alpha = 0
@@ -73,17 +90,41 @@ class ScheduleAppointmentController: UIViewController, FSCalendarDataSource, FSC
             self.slot1_button.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
             self.slot2_button.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
             self.slot3_button.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+            self.timeTravelLabel.alpha = 0
+            self.timeTravelLabel.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
             
         }, completion: { (Bool) in
-            self.showAllSlotButtons()
+            if(reverse){
+                self.showAllSlotButtons()
+            }
+            else{
+                self.showTimeTravelLabel()
+            }
+            
         })
     }
     
     // Calender Date Selection Delegate
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-       self.hideAllSlotButtons()
+        
+        let today = Date()
+        
+        let order = NSCalendar.current.compare(today, to: date, toGranularity: .day)
+        
+        
+        if(date < today && order == .orderedDescending){
+            print(date)
+            print(today)
+            self.hideAllSlotButtons(reverse: false)
+            
+        }
+        else{
+            self.hideAllSlotButtons(reverse: true)
+        }
+        
+        
     }
-
+    
     
     
     
